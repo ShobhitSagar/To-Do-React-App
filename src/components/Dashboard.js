1d63a7b4e1a266575aa05773b41fdf51374e88f4
ShobhitSagar <shobhitsagar109@gmail.com>
2020-11-04 20:21:19 +0530

    Made correction to sidbar toggle button. Add display property on @media width.

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,22 +1,28 @@
-import React, { useEffect, useRef, useState } from 'react';
-import './Dashboard.css'
+import React, { useEffect, useRef } from "react";
+import "./styles/Dashboard.css";
 import ToDos from "./ToDos";
 
-function Dashboard({barClass}) {
-
+function Dashboard({ barClass }) {
   const sidebarRef = useRef();
 
   useEffect(() => {
-    sidebarRef.current.style.display = barClass === "list-ul" ? "none" : "block";
-    if (window.innerWidth < 670)
-      sidebarRef.current.style.position = "absolute";
-  })
+    sidebarRef.current.style.display =
+      barClass === "list-ul" ? "none" : "block";
+    // if (window.innerWidth < 670) sidebarRef.current.style.position = "absolute";
+  });
 
   return (
     <section id="dashboard">
+      {/* SIDEBAR */}
       <section ref={sidebarRef} id="sidebar">
         <section id="search_section">
-          <input id="search-input" type="text" autoComplete="off" spellCheck="false" placeholder="Search..." />
+          <input
+            id="search-input"
+            type="text"
+            autoComplete="off"
+            spellCheck="false"
+            placeholder="Search..."
+          />
           <button className="fas fa-search"></button>
         </section>
 
@@ -25,12 +31,12 @@ function Dashboard({barClass}) {
           <button className="fas fa-plus-square"></button>
         </div>
 
-        <ul>
+        <ul id="all-todo-list">
           <li className="active">Today</li>
-          <li>List 2</li>
-          <li>List 3</li>
-          <li>List 4</li>
-          <li>List 5</li>
+          <li>Tomorrow</li>
+          <li>Work</li>
+          <li>Shopping</li>
+          <li>Wedding</li>
         </ul>
       </section>
 
--- src/components/ToDos.js
+++ src/components/ToDos.js
@@ -1,24 +1,29 @@
-import React, { useState } from 'react';
-import './ToDos.css'
-import NewTask from './NewTask';
-import TaskList from './TaskList';
+import React, { useState } from "react";
+import "./styles/ToDos.css";
+import NewTask from "./NewTask";
+import TaskList from "./TaskList";
 
 function Todos() {
-
   const [inputText, setInputText] = useState("");
   const [todos, setTodos] = useState([]);
 
-    return (
-      <section id="to_do_section">
-        <div>
-            <span id="list_name">Today</span>
-            <div id="divider"></div>
+  return (
+    <section id="to_do_section">
+      <div>
+        <span id="list_name">Today</span>
+        <div id="divider"></div>
 
-            <NewTask inputText={inputText} todos={todos} setInputText={setInputText} setTodos={setTodos} />
-            <TaskList todos={todos} setTodos={setTodos} />
-        </div>
-      </section>
-    );
+        <NewTask
+          inputText={inputText}
+          todos={todos}
+          setInputText={setInputText}
+          setTodos={setTodos}
+        />
+        <TaskList todos={todos} setTodos={setTodos} />
+      </div>
+      {/* <button id="sidebar-toggle" className="fas fa-arrow-circle-left"></button> */}
+    </section>
+  );
 }
 
 export default Todos;
