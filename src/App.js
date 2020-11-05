4c1ac13336835d1080edf80a449556b707d9e813
ShobhitSagar <shobhitsagar109@gmail.com>
2020-11-05 06:09:11 +0530

    Can add new todo list

--- src/App.js
+++ src/App.js
@@ -3,52 +3,52 @@ import React, { useEffect, useRef, useState } from "react";
 import Dashboard from "./components/Dashboard";
 
 function App() {
-  // const [sideBar, setSideBar] = useState("block");
-  const [barClass, setBarClass] = useState("times");
-  const barRef = useRef();
+	// const [sideBar, setSideBar] = useState("block");
+	const [barClass, setBarClass] = useState("times");
+	const barRef = useRef();
 
-  //   useEffect(() => {
-  //     if (window.innerWidth < 670) {
-  //       setBarClass("list-ul");
-  //       // setSideBar(barClass === "list-ul" ? "none" : "block");
-  //     } else {
-  //       barRef.current.style.display = "none";
-  //     }
-  //   }, []);
+	//   useEffect(() => {
+	//     if (window.innerWidth < 670) {
+	//       setBarClass("list-ul");
+	//       // setSideBar(barClass === "list-ul" ? "none" : "block");
+	//     } else {
+	//       barRef.current.style.display = "none";
+	//     }
+	//   }, []);
 
-  const openSideBar = () => {
-    if (barClass === "list-ul") {
-      setBarClass("times");
-      // setSideBar("block");
-    } else {
-      setBarClass("list-ul");
-      // setSideBar("none");
-    }
-  };
+	const openSideBar = () => {
+		if (barClass === "list-ul") {
+			setBarClass("times");
+			// setSideBar("block");
+		} else {
+			setBarClass("list-ul");
+			// setSideBar("none");
+		}
+	};
 
-  return (
-    <div className="App">
-      <nav id="navbar">
-        <button
-          onClick={openSideBar}
-          ref={barRef}
-          id="bar"
-          className={`fas fa-${barClass}`}
-        ></button>
-        <strong>
-          <a id="navbar-name" href="/">
-            To do
-          </a>
-        </strong>
-        <div></div>
-      </nav>
+	return (
+		<div className="App">
+			<nav id="navbar">
+				<button
+					onClick={openSideBar}
+					ref={barRef}
+					id="bar"
+					className={`fas fa-${barClass}`}
+				></button>
+				<strong>
+					<a id="navbar-name" href="/">
+						<i className="fas fa-clipboard-list"></i> To do
+					</a>
+				</strong>
+				<div></div>
+			</nav>
 
-      <section>
-        <Dashboard barClass={barClass} />
-        {/* <ToDoList /> */}
-      </section>
-    </div>
-  );
+			<section>
+				<Dashboard barClass={barClass} />
+				{/* <ToDoList /> */}
+			</section>
+		</div>
+	);
 }
 
 export default App;
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,48 +1,65 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./styles/Dashboard.css";
 import ToDos from "./ToDos";
 
 function Dashboard({ barClass }) {
-  const sidebarRef = useRef();
+	const [todoList, setTodoList] = useState([]);
+	const sidebarRef = useRef();
 
-  useEffect(() => {
-    sidebarRef.current.style.display =
-      barClass === "list-ul" ? "none" : "block";
-    // if (window.innerWidth < 670) sidebarRef.current.style.position = "absolute";
-  });
+	const addNewList = () => {
+		const newListName = prompt("Add new list name:");
+		if (newListName !== "") {
+			setTodoList([
+				...todoList,
+				{
+					name: newListName,
+					id: Date.now(),
+				},
+			]);
+		}
+	};
 
-  return (
-    <section id="dashboard">
-      {/* SIDEBAR */}
-      <section ref={sidebarRef} id="sidebar">
-        <section id="search_section">
-          <input
-            id="search-input"
-            type="text"
-            autoComplete="off"
-            spellCheck="false"
-            placeholder="Search..."
-          />
-          <button className="fas fa-search"></button>
-        </section>
+	useEffect(() => {
+		sidebarRef.current.style.display =
+			barClass === "list-ul" ? "none" : "block";
+		// if (window.innerWidth < 670) sidebarRef.current.style.position = "absolute";
+	});
 
-        <div id="add_list">
-          <h1>All List</h1>
-          <button className="fas fa-plus-square"></button>
-        </div>
+	return (
+		<section id="dashboard">
+			{/* SIDEBAR */}
+			<section ref={sidebarRef} id="sidebar">
+				<section id="search_section">
+					<input
+						id="search-input"
+						type="text"
+						autoComplete="off"
+						spellCheck="false"
+						placeholder="Search..."
+					/>
+					<button className="fas fa-search"></button>
+				</section>
 
-        <ul id="all-todo-list">
-          <li className="active">Today</li>
-          <li>Tomorrow</li>
-          <li>Work</li>
-          <li>Shopping</li>
-          <li>Wedding</li>
-        </ul>
-      </section>
+				<div id="add_list">
+					<h1>All List</h1>
+					<button onClick={addNewList} className="fas fa-plus-square"></button>
+				</div>
 
-      <ToDos />
-    </section>
-  );
+				<ul id="all-todo-list">
+					<li className="active">Today</li>
+					<li>Tomorrow</li>
+					<li>Work</li>
+					<li>Shopping</li>
+					<li>Wedding</li>
+					{todoList.map((list) => (
+						<li key={list.id}>{list.name}</li>
+					))}
+				</ul>
+			</section>
+
+			<ToDos />
+		</section>
+	);
 }
 
 export default Dashboard;
